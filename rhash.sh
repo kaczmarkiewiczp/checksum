7d2dfea608cf35f1134c7c6d593e5e93a2574c2c
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2017-02-18 19:12:56 -0700

    Print check summary on the same line as progress

--- rhash.sh
+++ rhash.sh
@@ -2,7 +2,7 @@
 # rhash: recursively scan specified directory for files. Create a hash of all
 # the files and append them to a specified output file
 
-VERSION=2.0.0 # program version
+VERSION=2.0.1 # program version
 EXE="$(basename $0)" # program name
 DEBUG=false # debugging mode
 HASH_COMMAND="" # command with appropriate flags used for hashing (i.e md5sum)
@@ -478,6 +478,7 @@ function print_check_summary() {
 	failed_num=${#failed_files[@]}
 	missing_num=${#missing_files[@]}
 
+	echo -en "\r\033[K"
 	echo -n "PASSED: $passed_num"
 	echo -n "   FAILED: $failed_num"
 	if [ $FLAG_IGNORE_MISS = false ]; then
@@ -560,14 +561,15 @@ function check() {
 		fi
 	done < "$1"
 
-	if [ $FLAG_PROGRESS = true ] && [ $FLAG_QUIET = false ]; then
+	if [ $FLAG_PROGRESS = true ] && [ $FLAG_SUMMARY = false ] && 
+	   [ $FLAG_QUIET = false ]; then
 		echo
 	fi
 
-	if [ $FLAG_QUIET = true -o $FLAG_SUMMARY = false ]; then
-		return
+	if [ $FLAG_QUIET = false -a $FLAG_SUMMARY = true ]; then
+		print_check_summary $ok_num $error_num failed_files[@] \
+				    missing_files[@]
 	fi
-	print_check_summary $ok_num $error_num failed_files[@] missing_files[@]
 
 	if [ ${#failed_files[@]} -gt 0 ]; then
 		return 200
